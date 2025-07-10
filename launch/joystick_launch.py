4c134de91eebff6ae757d9c823ac8ad2874f4ea1
shinku <shinku_1314@qq.com>
2025-07-10 14:09:52 +0800

    feat: 更新 launch 文件

--- launch/joystick_launch.py
+++ launch/joystick_launch.py
@@ -1,54 +1,45 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    # 声明并获取参数
-    ip_arg = DeclareLaunchArgument(
-        'ip', default_value='192.168.4.1',
-        description='IP address for the joystick publisher')
+    joystick_topic = "/joystick"
+    joystick_serial_port_name = "/dev/joystick_usb2ttl"
+    R1_service_name = "R1_srv"
+    R2_service_name = "R2_srv"
+    go_service_name = "go_srv"
+    joy_service_name = "joyvel_srv"
 
-    port_arg = DeclareLaunchArgument(
-        'port', default_value='3456',
-        description='Port for the joystick publisher')
-
-    serial_port_arg = DeclareLaunchArgument(
-        'serial_port', default_value='/dev/usb2ttl',
-        description='Serial port for the joystick publisher')
-    
-    controller_arg = DeclareLaunchArgument(
-        'controller', default_value='false',
-        description='Controller for the Chassis')
-
-    # 创建节点配置
-    joystick_publisher_node = Node(
-        package='joystick_upper_software',
-        executable='joystick_publisher',
-        name='joystick_publisher',
-        output='screen',
-        parameters=[{
-            'ip': LaunchConfiguration('ip'),
-            'port': LaunchConfiguration('port'),
-            'serial_port': LaunchConfiguration('serial_port')
-        }]
+    return LaunchDescription(
+        [
+            Node(
+                package="joystick_upper_software",
+                executable="joystick_publisher",
+                name="joystick_publisher",
+                output="log",
+                parameters=[{"serial_port": joystick_serial_port_name}],
+            ),
+            Node(
+                package="joystick_upper_software",
+                executable="joystick_handle_controller",
+                name="joystick_handle_controller",
+                output="log",
+                parameters=[{}],
+            ),
+            Node(
+                package="joystick_upper_software",
+                executable="joystick_button_controller",
+                name="joystick_button_controller",
+                output="log",
+                parameters=[
+                    {
+                        "joystick_topic": joystick_topic,
+                        "R1_service": R1_service_name,
+                        "R2_service": R2_service_name,
+                        "go_service": go_service_name,
+                        "joy_service": joy_service_name,
+                    }
+                ],
+            ),
+        ]
     )
-
-    joystick_controller_node = Node(
-        package="joystick_upper_software",
-        executable="joystick_controller",
-        name="joystick_controller",
-        output="screen",
-        condition=IfCondition(LaunchConfiguration("controller")),
-    )
-
-    # 创建并返回launch描述
-    return LaunchDescription([
-        ip_arg,
-        port_arg,
-        serial_port_arg,
-        controller_arg,
-        joystick_publisher_node,
-        joystick_controller_node
-    ])
